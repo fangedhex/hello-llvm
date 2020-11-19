b61d9c406d39732288df5bdd183bb0feb9645b79
Sylvain VISTE <viste.sylvain@gmail.com>
2020-11-19 15:12:18 +0100

    Generate object file and link it to an executable

--- Makefile
+++ Makefile
@@ -1,9 +1,14 @@
 CXX=clang++
-CXX_FLAGS=$(shell llvm-config --cxxflags) -Wall
-LD_FLAGS=$(shell llvm-config --libs)
+FLAGS=$(shell llvm-config --cxxflags --ldflags --system-libs --libs all) -Wall
 
 all:
-	$(CXX) -o hello $(CXX_FLAGS) $(LD_FLAGS) src/main.cpp
+	$(CXX) -o helloc $(FLAGS) src/main.cpp
+	@echo "---------------------------------"
+	./helloc
+	@echo "---------------------------------"
+	ld -dynamic-linker /lib/ld-linux-x86-64.so.2 -o hello_world -lc output.o
+	@echo "---------------------------------"
+	./hello_world
 
 clean:
 	rm -rf ./build
--- src/main.cpp
+++ src/main.cpp
@@ -10,42 +10,62 @@
 #include <llvm/ExecutionEngine/Orc/ObjectLinkingLayer.h>
 #include <llvm/ExecutionEngine/Orc/IRCompileLayer.h>
 #include <llvm/ExecutionEngine/GenericValue.h>
+#include <llvm/Support/Host.h>
+#include <llvm/Support/TargetRegistry.h>
+#include <llvm/Support/FileSystem.h>
+#include <llvm/IR/LegacyPassManager.h>
 
 typedef llvm::orc::ObjectLinkingLayer ObjLayerT;
 typedef llvm::orc::IRCompileLayer CompileLayerT;
 
+#define MAIN_FUNC "_start"
+
 int main(int argc, char const *argv[])
 {
+  /**
+   * LLVM IR Code generation
+   */
   llvm::LLVMContext context;
   llvm::Module module("top", context);
   llvm::IRBuilder<> builder(context);
 
-  auto funcType = llvm::FunctionType::get(builder.getInt32Ty(), false);
-  auto mainFunc = llvm::Function::Create(funcType, llvm::Function::ExternalLinkage, "main", module);
+  auto funcType = llvm::FunctionType::get(builder.getVoidTy(), false);
+  auto mainFunc = llvm::Function::Create(funcType, llvm::Function::ExternalLinkage, MAIN_FUNC, module);
 
   llvm::BasicBlock *entry = llvm::BasicBlock::Create(context, "entrypoint", mainFunc);
   builder.SetInsertPoint(entry);
 
   llvm::Value *helloWorld = builder.CreateGlobalStringPtr("hello world!\n");
 
+  // We "import" the puts function
   std::vector<llvm::Type *> putsArgs;
   putsArgs.push_back(builder.getInt8Ty()->getPointerTo());
-  llvm::ArrayRef<llvm::Type *> argsRef(putsArgs);
-
-  auto putsType = llvm::FunctionType::get(builder.getInt32Ty(), argsRef, false);
+  auto putsType = llvm::FunctionType::get(builder.getInt32Ty(), putsArgs, false);
   auto putsFunc = module.getOrInsertFunction("puts", putsType);
 
+  // We "import" the exit function
+  std::vector<llvm::Type *> exitArgs;
+  exitArgs.push_back(builder.getInt32Ty());
+  auto exitType = llvm::FunctionType::get(builder.getVoidTy(), exitArgs, false);
+  auto exitFunc = module.getOrInsertFunction("exit", exitType);
+
   builder.CreateCall(putsFunc, helloWorld);
+  builder.CreateCall(exitFunc, builder.getInt32(0));
   builder.CreateRetVoid();
 
   module.print(llvm::errs(), nullptr);
 
-  // Initilaze native target
-  llvm::InitializeNativeTarget();
-  llvm::InitializeNativeTargetAsmPrinter();
-  llvm::InitializeNativeTargetAsmParser();
+  llvm::InitializeAllTargetInfos();
+  llvm::InitializeAllTargets();
+  llvm::InitializeAllTargetMCs();
+  llvm::InitializeAllAsmParsers();
+  llvm::InitializeAllAsmPrinters();
 
-  std::unique_ptr<llvm::Module> ptrModule(&module);
+  /**
+   * Running our generated IR code directly here
+   */
+
+  /*std::unique_ptr<llvm::Module> ptrModule(&module);
   llvm::EngineBuilder engineBuilder(std::move(ptrModule));
   engineBuilder.setEngineKind(llvm::EngineKind::Interpreter);
 
@@ -53,10 +73,60 @@ int main(int argc, char const *argv[])
   engineBuilder.setErrorStr(&err);
   auto executionEngine = engineBuilder.create();
   executionEngine->runStaticConstructorsDestructors(false);
-  auto main = executionEngine->FindFunctionNamed("main");
+  auto main = executionEngine->FindFunctionNamed(MAIN_FUNC);
   if (!main)
     return EXIT_FAILURE;
-  executionEngine->runFunction(main, llvm::ArrayRef<llvm::GenericValue>());
+  executionEngine->runFunction(main, llvm::ArrayRef<llvm::GenericValue>());*/
+
+  /**
+   * Generating an object file and linking it through ld
+   */
+
+  auto targetTriple = llvm::sys::getDefaultTargetTriple();
+
+  std::string Error;
+  auto target = llvm::TargetRegistry::lookupTarget(targetTriple, Error);
+
+  // Print an error and exit if we couldn't find the requested target.
+  // This generally occurs if we've forgotten to initialise the
+  // TargetRegistry or we have a bogus target triple.
+  if (!target)
+  {
+    llvm::errs() << Error;
+    return 1;
+  }
+
+  auto CPU = "generic";
+  auto Features = "";
+
+  llvm::TargetOptions opt;
+  auto RM = llvm::Optional<llvm::Reloc::Model>();
+  auto targetMachine = target->createTargetMachine(targetTriple, CPU, Features, opt, RM);
+
+  module.setDataLayout(targetMachine->createDataLayout());
+  module.setTargetTriple(targetTriple);
+
+  auto Filename = "output.o";
+  std::error_code EC;
+  llvm::raw_fd_ostream dest(Filename, EC, llvm::sys::fs::OF_None);
+
+  if (EC)
+  {
+    llvm::errs() << "Could not open file: " << EC.message();
+    return EXIT_FAILURE;
+  }
+
+  llvm::legacy::PassManager pass;
+  auto FileType = llvm::CGFT_ObjectFile;
+
+  if (targetMachine->addPassesToEmitFile(pass, dest, nullptr, FileType))
+  {
+    llvm::errs() << "TargetMachine can't emit a file of this type";
+    return EXIT_FAILURE;
+  }
+
+  pass.run(module);
+  dest.flush();
 
   return EXIT_SUCCESS;
 }
